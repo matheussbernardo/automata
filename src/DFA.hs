52c0a15efafdea76b61c33f75fd06ddf7ea7074d
Matheus Bernardo <matheussber@aluno.unb.br>
2019-04-03 18:41:20 -0300

    Adding support for reading DFA from file

--- src/DFA.hs
+++ src/DFA.hs
@@ -6,11 +6,15 @@ where
 
 data DFA st  = DFA
     { states :: [st]
-    , sigma :: [Char]
+    , sigma :: [String]
     , delta :: (st -> Char ->  st)
     , startState :: st
     , acceptStates :: [st]
     }
+
+instance (Show st) => Show (DFA st) where
+    show (DFA states sigma delta start accept) =
+        show states ++ show sigma ++ show start ++ show accept
 
 run :: Eq st => String -> DFA st -> Bool
 run word dfa = foldl (delta dfa) (startState dfa) word `elem` acceptStates dfa
--- src/Main.hs
+++ src/Main.hs
@@ -5,48 +5,43 @@ import           NFA                            ( NFA(..)
                                                 , run
                                                 )
 import qualified Data.Set                      as S
+import qualified Data.Map                      as Map
+import           Data.Maybe
+import           System.Environment
 
 main :: IO ()
 main = do
-    print (DFA.run "BABABA" exampleDFA)
-    print (NFA.run "A" exampleNFA)
+  filename            <- head <$> getArgs
+  automatoDescription <- lines <$> readFile filename
+  let automato = constructAutomato automatoDescription
+  let word     = last automatoDescription
 
+  print (DFA.run word automato)
 
-step' :: Integer -> Char -> Integer
-step' 0 'A' = 0
-step' 0 'B' = 1
-step' 1 'A' = 2
-step' 1 'B' = 0
-step' 2 'A' = 1
-step' 2 'B' = 2
+constructAutomato :: [String] -> DFA Int
+constructAutomato input = DFA [0 .. (states - 1)]
+                              symbols
+                              transitions
+                              initial
+                              accept
+ where
+  states  = read $ input !! 0 :: Int
+  size    = read $ take 1 (input !! 1) :: Int
+  symbols = words $ drop 1 (input !! 1)
+  transitions :: Int -> Char -> Int
+  transitions state symbol = fromMaybe
+    (-1)
+    (lookup (state, symbol)
+            (map transform ((take (states * size)) $ drop 2 input))
+    )
 
-exampleDFA :: DFA Integer
-exampleDFA = DFA [0, 1, 2] ['A', 'B'] step' 0 [0]
+  initial = read $ head $ drop (2 + (states * size)) input :: Int
+  accept  = map read (words $ head $ drop (3 + (states * size)) input)
 
-exampleDelta :: Integer -> Char -> S.Set Integer
-exampleDelta 0 'A' = S.fromList []
-exampleDelta 1 'A' = S.fromList [2]
-exampleDelta 2 'A' = S.fromList [3]
-exampleDelta 3 'A' = S.fromList []
-exampleDelta 4 'A' = S.fromList []
-exampleDelta 5 'A' = S.fromList []
-exampleDelta 0 'E' = S.fromList [0, 1, 4]
-exampleDelta 1 'E' = S.fromList [1]
-exampleDelta 2 'E' = S.fromList [2, 3]
-exampleDelta 3 'E' = S.fromList [3]
-exampleDelta 4 'E' = S.fromList [4]
-exampleDelta 5 'E' = S.fromList [5]
-exampleDelta 0 'B' = S.fromList []
-exampleDelta 1 'B' = S.fromList []
-exampleDelta 2 'B' = S.fromList []
-exampleDelta 3 'B' = S.fromList []
-exampleDelta 4 'B' = S.fromList [5]
-exampleDelta 5 'B' = S.fromList []
-
-
-exampleNFA :: NFA Integer
-exampleNFA = NFA (S.fromList [0, 1, 2, 3, 4, 5])
-                 ['A', 'B']
-                 exampleDelta
-                 0
-                 (S.fromList [3, 5])
+transform :: String -> ((Int, Char), Int)
+transform transition = ((state, symbol), dest)
+ where
+  arr    = words $ transition
+  state  = read $ arr !! 0
+  symbol = head $ arr !! 1
+  dest   = read $ arr !! 2
